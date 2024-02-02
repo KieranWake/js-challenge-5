128f60ec98a22a96e7ce8c243e679620b0a71f24
Kieran Wake <kieranwake1234@gmail.com>
2024-02-02 16:53:46 +0000

    wrote pseudocode

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 function whichIsLarger(f, g) {
+    //First, we instantiate two arrow functions inside our parent function -
+    //Use ternary operators with greater than operators to compare the two.
 	
 }
