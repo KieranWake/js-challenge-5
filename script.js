6e6e939541a3a3eebf3e5a103558d0d2e428e09e
Kieran Wake <kieranwake1234@gmail.com>
2024-02-05 17:14:49 +0000

    removed irrelevant arrow functions and called two empty functions in the conditional directly. 'g()' and 'f()'

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 function whichIsLarger(f, g) {
-    //First, we instantiate two arrow functions inside our parent function -
-    //Use ternary operators with greater than operators to compare the two.
-
-    const firstFunction = () => f; 
-    const secondFunction = () => g; 
-
-    if (f > g) {
+   
+    if (f() > g()) {
         return "f"; 
-    } else if (g > f) {
+    } else if (g() > f()) {
         return "g";
     } else {
         return "neither"; 
     }
 }
+
