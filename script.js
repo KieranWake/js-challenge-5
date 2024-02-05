fbf613db3d6941153741dfbc5a3b5f88ef2e591d
Kieran Wake <kieranwake1234@gmail.com>
2024-02-05 16:54:23 +0000

    proceeding to declare the two individual functions

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function whichIsLarger(f, g) {
     //First, we instantiate two arrow functions inside our parent function -
     //Use ternary operators with greater than operators to compare the two.
+
+    const 
 	
 }
