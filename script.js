a0f1c71a6329c38ffb438de149cc292c7f9dbbc4
Kieran Wake <kieranwake1234@gmail.com>
2024-02-05 17:04:54 +0000

    declared two const variables as arrow functions, equal to the original function parameters

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ function whichIsLarger(f, g) {
     //First, we instantiate two arrow functions inside our parent function -
     //Use ternary operators with greater than operators to compare the two.
 
-    const 
+    const firstFunction = () => f; 
+    const secondFunction = () => g; 
+
+    
+
+
 	
 }
